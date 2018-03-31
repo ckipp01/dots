9048298a611ac9f791fbaf7bf852b33f5f89bfb0
Chris <christopherkipp0@gmail.com>
2018-03-31 09:38:23 -0700

    added in green coloring for copied files

--- scripts/dot-backup.sh
+++ scripts/dot-backup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-for i in .vimrc .vimrc.plug .tmux.conf .tmux.snapshot .zshrc tmux-256color-italic.terminfo xterm-256color-italic.terminfo
+for i in .vimrc .vimrc.plug .tmux.conf \
+        .tmux.snapshot .zshrc tmux-256color-italic.terminfo \
+        xterm-256color-italic.terminfo
 do
     if [ -e ${HOME}/$i ]
     then
         [[ `diff ${HOME}/$i ${HOME}/dots/home/$i` ]] &&
-            (cp ${HOME}/$i ${HOME}/dots/home/$i && echo "$i copied") ||
+            (cp ${HOME}/$i ${HOME}/dots/home/$i && echo "\033[0;32m$i copied \033[0m") ||
                 (echo "$i was the same")
     else
         echo "no $i on this machine"
@@ -17,9 +19,10 @@ do
     if [ -e ${HOME}/bin/$w ]
     then
         [[ `diff ${HOME}/bin/$w ${HOME}/dots/scripts/$w` ]] &&
-            (cp ${HOME}/bin/$w ${HOME}/dots/scripts/$w && echo "$w copied") ||
+            (cp ${HOME}/bin/$w ${HOME}/dots/scripts/$w && echo "\033[0;32m$w copied \033[0m") ||
                 (echo "$w was the same")
     else
         echo "no $w on this machine"
     fi
 done
+
