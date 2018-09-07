99e1b5411388ba65a1275caf073acb3dc47a7f27
ckipp01 <christopherkipp0@gmail.com>
2018-09-07 08:28:32 +0200

    finishing up the backup script and making sure everything in the directory is up to date

--- scripts/dot-backup.sh
+++ scripts/dot-backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#files directly in ~
 for i in .vimrc .tmux.conf
 do
     if [ -e ${HOME}/$i ]
@@ -18,3 +19,60 @@ do
     fi
 done
 
+#flavor files
+for f in alias.bash function.bash tmux.snapshot vimrc.plug
+do
+  if [ -e ${HOME}/.flavor/$f ]
+  then
+    if [ ! -e ${HOME}/dots/tilde/flavor/$f ]
+    then
+      cp ${HOME}/.flavor/$f ${HOME}/dots/tilde/flavor/$f && echo "$f is now being tracked"
+    elif [ ${HOME}/.flavor/$f -nt ${HOME}/dots/tilde/flavor/$f ]
+    then
+        cp ${HOME}/.flavor/$f ${HOME}/dots/tilde/flavor/$f && echo "$f copied"
+    else
+      echo "$f was the same"
+    fi
+  else
+      echo "no $f on this machine"
+  fi
+done
+
+#config directory files
+for c in termite/config gtk-3.0/gtk.css
+do
+  if [ -e ${HOME}/.config/$c ]
+  then
+    if [ ! -e ${HOME}/dots/tilde/config/$c ]
+    then
+      cp ${HOME}/.config/$c ${HOME}/dots/tilde/config/$c && echo "$c is now being tracked"
+    elif [ ${HOME}/.config/$c -nt ${HOME}/dots/tilde/config/$c ]
+    then
+        cp ${HOME}/.config/$c ${HOME}/dots/tilde/config/$c && echo "$c copied"
+    else
+      echo "$c was the same"
+    fi
+  else
+      echo "no $c on this machine"
+  fi
+done
+
+
+#scripts
+for s in dot-backup.sh tmux-work.sh
+do
+  if [ -e ${HOME}/bin/$s ]
+  then
+    if [ ! -e ${HOME}/dots/scripts/$s ]
+    then
+      cp ${HOME}/bin/$s ${HOME}/dots/scripts/$s && echo "$s is now being tracked"
+    elif [ ${HOME}/bin/$s -nt ${HOME}/dots/scripts/$s ]
+    then
+        cp ${HOME}/bin/$s ${HOME}/dots/scripts/$s && echo "$s copied"
+    else
+      echo "$s was the same"
+    fi
+  else
+      echo "no $s on this machine"
+  fi
+done
