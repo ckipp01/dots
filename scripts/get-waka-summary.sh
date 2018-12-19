fda2ddc4c187e43ad36c576d92200eeff461676e
ckipp01 <christopherkipp0@gmail.com>
2018-12-19 13:27:34 +0100

    adding in ability to just get the grand total in get-waka script

--- scripts/get-waka-summary.sh
+++ scripts/get-waka-summary.sh
@@ -5,7 +5,7 @@ baseurl="https://wakatime.com/api/v1/"
 user="ckipp01"
 outputDir=${HOME}/Documents/waka-records/
 
-echo -e "${BLUE}Would you like to provide a date or range? (d/r)${END}"
+echo -e "${BLUE}Would you like to provide a date or a range? (d/r)${END}"
 read dateFormat
 
 if [ $dateFormat = "d" ]
@@ -16,17 +16,17 @@ then
   endDate=$date
 elif [ $dateFormat = "r" ]
 then
-  echo -e "${LBLUE}Provide start date (YYYY-MM-DD): ${END}"
+  echo -e "${BLUE}Provide start date (YYYY-MM-DD): ${END}"
   read startDate
 
-  echo -e "${LBLUE}Provide end date (YYYY-MM-DD): ${END}"
+  echo -e "${BLUE}Provide end date (YYYY-MM-DD): ${END}"
   read endDate
 else
   echo -e ${GREEN}Ok, bye.${END}
   exit 0
 fi
 
-echo -e "${LBLUE}Would you like to print results to file or terminal? (f/t)${END}"
+echo -e "${BLUE}Would you like to print results to file or terminal? (f/t)${END}"
 read FT
 case ${FT} in
   [Ff]* )
@@ -52,8 +52,15 @@ then
       echo $response
       ;;
     terminal)
+      echo -e "${BLUE}Would you like a full report or just the grand total? (f/gt)${END}"
+      read terminalReportType
       response=$(curl -v --header "Authorization: Basic ${encoded}" "${baseurl}users/${user}/summaries?start=${startDate}&end=${endDate}")
-      echo $response | jq
+      if [ $terminalReportType = 'f' ]
+      then
+        echo $response | jq
+      else
+        echo $response | jq '.data[0].grand_total'
+      fi
       ;;
   esac
 else
