39c30cabffb74ca4ccd37abb4b4f4152e5423654
ckipp01 <ckipp@pm.me>
2021-12-26 17:02:32 +0100

    Clean up old scripts

---
+++ bin/bin/clean-bsp-workspace.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+rm -rf $PWD/.bloop/
+rm -rf $PWD/.metals/
+rm -rf $PWD/.bsp/
+rm -rf $PWD/.ammonite
+rm -rf $PWD/project/project/
+rm -rf $PWD/project/target/
+rm -rf $PWD/project/.bloop/
+rm -rf $PWD/target/
+rm -rf $PWD/out/
+rm -f $PWD/project/metals.sbt
--- tmux/bin/tm.sh
+++ tmux/bin/tm.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 ###########################################################
 # General startup of tmux with my most common setup
 ###########################################################
@@ -9,13 +8,13 @@ session="main"
 
 tmux start-server
 
-tmux new-session -d -s $session -n "config"
-tmux send-keys "cd ~/.config/nvim" C-m
+tmux new-session -d -s $session -n "dots"
+tmux send-keys "cd ~/dots/nvim/.config/nvim" C-m
 
 tmux new-window -t $session:1 -n "notes"
-tmux send-keys "cd ~/Documents/notes && v todo.md" C-m
+tmux send-keys "cd ~/Documents/notes" C-m
 
-tmux new-window -t $session:2 -n "scratch"
+tmux new-window -t $session:2
 tmux send-keys "cd ~/Documents/scratch-workspace" C-m
 
 # select window to start 
